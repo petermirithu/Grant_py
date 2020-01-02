063d0da9f75197d3e426637b2205ea4fafbc6e33
petermirithu <pyra_m.k@yahoo.com>
2020-01-02 11:21:11 +0300

    register signals

--- awards/apps.py
+++ awards/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class AwardsConfig(AppConfig):
     name = 'awards'
+
+    def ready(self):
+        '''
+        function that helps in importing the signals to auto create a profile for a new user
+        '''
+        import awards.signals
