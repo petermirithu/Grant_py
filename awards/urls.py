4ce9e8cf3b7ab5370e68e852e8cd13e12b9db857
petermirithu <pyra_m.k@yahoo.com>
2020-01-03 17:21:02 +0300

    add url for single post,new post, profile page & update profile

--- awards/urls.py
+++ awards/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     url(r'^logout/$',views.logout_request,name="logout"),
     url(r'^accounts/profile/$',views.user_profile,name='profile'),    
     url(r'^update/profile/$',views.update_profile,name='updateprofile'),
+    url(r'^new_post/$',views.post_project,name="new_post"),
+    path(r'single_post/<int:post_id>',views.single_post,name="single_post"),
+    path(r'update_post/<int:post_id>',views.update_post,name="update_post"),
+    path(r'profile/<username>',views.other_user_profile,name="user_profile"),
 ]
