43307f347333b24f6d2076cc28e75cfcdfbda76a
petermirithu <pyra_m.k@yahoo.com>
2020-01-04 19:10:38 +0300

    add models to admin site

--- awards/admin.py
+++ awards/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import profile
+from .models import profile,projo_post,reviews,preference
 
 # Register your models here.
 admin.site.register(profile)
+admin.site.register(projo_post)
+admin.site.register(preference)
+admin.site.register(reviews)
+
