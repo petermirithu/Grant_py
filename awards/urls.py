157b7267e323da7401589ee5f9ea27623a5a9ad3
petermirithu <pyra_m.k@yahoo.com>
2020-01-08 12:34:48 +0300

    remove paramenters for ajax urls

--- awards/urls.py
+++ awards/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path(r'single_post/<int:post_id>',views.single_post,name="single_post"),
     path(r'update_post/<int:post_id>',views.update_post,name="update_post"),
     path(r'profile/<username>',views.other_user_profile,name="user_profile"),
-    path(r'ajax/review/<int:projo_id>',views.add_review,name="add_review"),
-    path(r'rate/<int:post_id>/<rated>',views.rate,name="rate"),
+    url(r'^ajax/review/$',views.add_review,name="add_review"),
+    path(r'rate/',views.rate,name="rate"),
     url(r'^search/$',views.search,name="search"),
     path(r'delete/<int:post_id>',views.delete_project,name="delete"),    
     # api end points
