8daeee5b050e14a708d7abaa189acd5389b10dcb
petermirithu <pyra_m.k@yahoo.com>
2020-01-08 13:15:51 +0300

    correct allowedhosts

--- grant_py/settings.py
+++ grant_py/settings.py
@@ -112,7 +112,7 @@ else:
     }
     db_from_env=dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
-    ALLOWED_HOST=config('ALLOWED_HOST',cast=Csv())    
+    ALLOWED_HOSTS=config('ALLOWED_HOSTS',cast=Csv())    
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
